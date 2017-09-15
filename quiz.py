e018e2844923a91499977bab4ca3e3ebedc3af09
alia <jannatulalia.ramli@onapp.com>
2017-09-15 14:20:36 +0800

    tipu sangat

---
+++ quiz.py
@@ -0,0 +1,12 @@
+def statement():
+    print "Hello Dearest World, i have a quiz for ya"
+    print "Where is the location of malaysia?"
+
+def main():
+    statement()
+
+
+
+if __name__ == "__main__":
+   main()
+
